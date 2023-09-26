856df82ea82ed098dbcd1fa98c89d5fb87beaeef
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-26 21:27:38 +0200

    Create sanity config

---
+++ sanity.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'sanity'
+import {deskTool} from 'sanity/desk'
+
+const config = defineConfig({
+    projectId: 'l2yq8lmm',
+    dataset: 'production',
+    title: 'next-sanity-studio',
+    apiVersion: '2023-09-23',
+    basePath: '/admin', 
+    plugins: [deskTool()]
+})
