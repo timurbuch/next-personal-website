c5fe4dde0b74e3c5ae8740427703d24002d26b88
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-27 22:22:08 +0200

    Add getPages and getPage function

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,4 +1,5 @@
 import { Project } from "@/types/Project";
+import { Page } from "@/types/Page";
 import { createClient} from "@sanity/client";
 import clientConfig from './config/client-config'
 
@@ -29,3 +30,24 @@ export async function getProject(slug: string): Promise<Project> {
     )
 }
 
+export async function getPages(): Promise<Page[]> {
+    return createClient(clientConfig).fetch(`*[_type == "page"]{
+        _id,
+        _createdAt,
+        title,
+        "slug": slug.current
+    }`)
+}
+
+export async function getPage(slug: string): Promise<Page> {
+    return createClient(clientConfig).fetch(`*[_type == "page" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        "slug": slug.current,
+        content
+      }`, 
+    {slug}
+    )
+}
+   
