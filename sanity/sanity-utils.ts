813a781cf6ab738158f892d2e4896101d7de7ae8
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-27 13:43:24 +0200

    Add getProject function to get project details

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -22,3 +22,25 @@ export async function getProjects(): Promise<Project[]> {
     }`)
 }
 
+export async function getProject(slug: string): Promise<Project> {
+    const client = createClient({
+        projectId: 'l2yq8lmm',
+        dataset: 'production',
+        apiVersion: '2023-09-23',
+        useCdn: false
+    })
+
+    return client.fetch(`*[_type == "project && slug.current == ${slug}[0]"]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset -> url,
+        alt,
+        url,
+        content
+    }`, 
+    {slug}
+    )
+}
+
