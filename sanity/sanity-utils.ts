1cc47d1c143cad049ab0e6860e0ac481fad9e5fc
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-27 10:29:37 +0200

    Add alt property to utils and project type

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -16,6 +16,7 @@ export async function getProjects(): Promise<Project[]> {
         name,
         "slug": slug.current,
         "image": image.asset -> url,
+        alt,
         url,
         content
     }`)
