a7f77e66adfafcd08781522b9e48f0e59f7366e7
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-27 16:35:13 +0200

    Style content section of project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,7 +1,10 @@
 import { getProject } from "@/sanity/sanity-utils"
+import { PortableText } from "@portabletext/react"
+import Image from "next/image"
 
 type Props = {
     params: {project:string}
+    
 }
 
 
@@ -14,5 +17,15 @@ export default async function Project({params}: Props) {
             <h1 className="bg-gradient-to-r from-amber-400 via-purple-500 to bg-red-400 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">{project.name}</h1>
             <a href={project.url} title="View Project" target="_blank" rel="noopener noreferrer" className="bg-blue-600 rounded-lg text-blue-50 font-bold py-3 px-4 whitespace-nowrap hover:bg-blue-100 hover:text-blue-900 transition">View Project</a>
         </header>
+        <div className="text-lg text-slate-100 mt-5">
+            <PortableText value={project.content}/>
+        </div>
+        {project.image && (
+             <Image src={project.image} alt={project.image} width={1920} height={1080} className="mt-10 border-2 border-slate-200 object-cover rounded-xl"/>
+        )
+
+        }
+       
+
         </div>
 }
